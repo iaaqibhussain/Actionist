32908e1787daa053cc3fc1e23f046b172fed59f3
Aaqib Hussain <iaaqibhussain@outlook.com>
2025-03-02 11:40:36 +0100

    add test

--- ActionistTests/ActionistFeatureTests.swift
+++ ActionistTests/ActionistFeatureTests.swift
@@ -12,29 +12,22 @@ import Testing
 @testable import Actionist
 
 @MainActor
-struct Actio {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+struct ActionistFeatureTests {
+    
+    @Test
+    func addItem() async {
+        let store = TestStore(initialState: ActionistFeature.State()) {
+            ActionistFeature()
+           } withDependencies: {
+             $0.uuid = .incrementing
+           }
+        
+        await store.send(.add("New Task")) {
+            $0.items.append(ActionItem(id: UUID(0), title: "New Task"))
+        }
+        
+        await store.send(.add("New Task 1")) {
+            $0.items.append(ActionItem(id: UUID(1), title: "New Task 1"))
         }
     }
-
 }
